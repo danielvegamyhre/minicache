903e140d2d35475846fbd04c5cc94e5c2897c7bd
Daniel Vega-Myhre <dvegamyhre@gmail.com>
2022-05-06 15:02:13 -0600

    fix bug

--- main.go
+++ main.go
@@ -10,14 +10,16 @@ import (
 
 func main() {
 	// parse arguments
-	port := flag.Int("port", 8080, "port number for server to listen on")
+	grpc_port := flag.Int("grpc-port", 5005, "port number for gRPC server to listen on")
 	capacity := flag.Int("capacity", 2, "capacity of LRU cache")
 	verbose := flag.Bool("verbose", false, "log events to terminal")
 	config_file := flag.String("config", "", "filename of JSON config file with node info")
+	rest_port := flag.Int("rest-port", 8080, "enable REST API for client requests, instead of just gRPC")
+
 	flag.Parse()
 
 	// set up listener TCP connectiion
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *grpc_port))
 	if err != nil {
 		panic(err)
 	}
@@ -26,7 +28,7 @@ func main() {
 	grpc_server, cache_server := server.GetNewCacheServer(*capacity, *config_file, *verbose)
 
 	// run gRPC server
-	log.Printf("Running gRPC server on port %d...", 5005)
+	log.Printf("Running gRPC server on port %d...", *grpc_port)
 	go grpc_server.Serve(listener)
 
 	// run initial election
@@ -36,5 +38,5 @@ func main() {
 	go cache_server.StartLeaderHeartbeatMonitor()
 
 	// run HTTP server
-	cache_server.RunHttpServer(*port)
+	cache_server.RunHttpServer(*rest_port)
 }
