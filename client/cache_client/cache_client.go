1c8a5cf93d7cd1eda40086aec3d34824c0787194
malwaredllc <30509968+malwaredllc@users.noreply.github.com>
2022-05-07 15:33:36 -0600

    updates

--- client/cache_client/cache_client.go
+++ client/cache_client/cache_client.go
@@ -7,8 +7,15 @@ import (
 	"net/http"
 	"encoding/json"
 	"io/ioutil"
+	"crypto/tls"
+	"crypto/x509"
+	"context"
 	"github.com/malwaredllc/minicache/node"
 	"github.com/malwaredllc/minicache/ring"
+	"github.com/malwaredllc/minicache/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+		
 )
 
 // Client wrapper for interacting with Identity Server
@@ -89,4 +96,50 @@ func (c *ClientWrapper) Put(key string, value string) string {
         log.Fatal(err)
     }
 	return string(body)
+}
+
+// Utility funciton to get a new Cache Client which uses gRPC secured with mTLS
+func NewCacheClient(server_host string, server_port int) pb.CacheServiceClient {
+	// set up TLS
+	creds, err := LoadTLSCredentials()
+	if err != nil {
+		log.Fatalf("failed to create credentials: %v", err)
+	}
+
+	// set up connection
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", server_host, server_port), grpc.WithTransportCredentials(creds))
+	if err != nil {
+		panic(err)
+	}
+
+	// set up client
+	return pb.NewCacheServiceClient(conn)
+}
+
+// Utility function to set up mTLS config and credentials
+func LoadTLSCredentials() (credentials.TransportCredentials, error) {
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile("certs/ca-cert.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Load client's certificate and private key
+	clientCert, err := tls.LoadX509KeyPair("certs/client-cert.pem", "certs/client-key.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      certPool,
+	}
+
+	return credentials.NewTLS(config), nil
 }
