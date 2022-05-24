6ac5ab054e2b5348c4a233213b3ed2268aadaf42
Daniel Vega-Myhre <dvegamyhre@gmail.com>
2022-05-24 16:02:08 -0600

    update docker tests and readme

--- docker-test.sh
+++ docker-test.sh
@@ -3,8 +3,11 @@
 # build client image
 docker build -t cacheclient -f Dockerfile.client . || { echo "failed to build docker image for cache client"; exit 1; }
 
+# build server image
+docker-compose build
+
 # spin up containerized cache servers using docker compose
-docker-compose start || { echo "docker-compose start failed"; exit 1; }
+docker-compose up -d --remove-orphans || { echo "docker-compose start failed"; exit 1; }
 
 # run tests in docker
 docker run --network minicache_default cacheclient || { echo "failed to run docker image cacheclient"; exit 1; }
